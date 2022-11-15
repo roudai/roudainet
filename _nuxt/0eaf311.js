5067b5551b466c0a8c88e68bfd01154fc6b2e78e
roudai <roudai@users.noreply.github.com>
2022-11-15 13:37:54 +0000

    deploy: 95f18e3a6dfc2db87024bf7080b884d47447f9ce

--- _nuxt/0eaf311.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{337:function(n,t,c){"use strict";c.r(t);var e=c(26),component=Object(e.a)({},(function(){return(0,this._self._c)("div",{staticClass:"content m-2"},[this._v("ルービックキューブ目隠しの解法")])}),[],!1,null,null,null);t.default=component.exports}}]);
--- _nuxt/static/1668514693/other/3x3x3-method/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1668514693",layout:"default",error:a,serverRendered:true,routePath:"\u002Fother\u002F3x3x3-method",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
