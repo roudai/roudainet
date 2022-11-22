11b327d54d8e908a07a6abff108db3c54f200067
roudai <me@roudai.net>
2022-11-22 16:07:44 +0900

    ページ幅変更

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div class="main-content columns ml-0 mr-0">
-      <div class="container column is-10-tablet is-6-desktop pl-0 pr-0">
+      <div class="container column is-10 pl-0 pr-0">
         <nav-bar></nav-bar>
         <div class="content m-3">
           <Nuxt />
